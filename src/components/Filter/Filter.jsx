27994b707936ed2e574c1f59df5c19490a94ff24
Alina-Pishak <alinapishak@icloud.com>
2023-10-25 16:07:01 +0300

    fix filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,27 @@
 import { setFilter } from 'redux/filter/filterSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Box, TextField } from '@mui/material';
+import { selectContacts } from 'redux/contact/selectors';
 
 const Filter = () => {
   const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
   const getFilterData = ({ target: { value } }) => {
     dispatch(setFilter(value));
   };
   return (
-    <Box noValidate sx={{ mt: 1 }}>
-      <TextField
-        fullWidth
-        type="text"
-        name="filter"
-        onChange={getFilterData}
-        label="Find contacts by Name"
-        variant="standard"
-      />
-    </Box>
+    contacts.length > 0 && (
+      <Box noValidate sx={{ mt: 1 }}>
+        <TextField
+          fullWidth
+          type="text"
+          name="filter"
+          onChange={getFilterData}
+          label="Find contacts by Name"
+          variant="standard"
+        />
+      </Box>
+    )
   );
 };
 
