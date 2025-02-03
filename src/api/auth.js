18ba92c4d8b2ac133ae0544f06239103600dce32
alina-pishak <alinapishak@icloud.com>
2025-02-03 10:37:07 +0200

    change endpoint

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://connections-api.goit.global/';
 
 export const setToken = token => {
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
